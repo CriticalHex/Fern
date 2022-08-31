7aeef8a636075652873c80859993b1feb3c22375
Terminal Tyrant <kubaska05@gmail.com>
2022-08-31 13:53:27 -0600

    faster

--- fern.py
+++ fern.py
@@ -5,10 +5,10 @@ import random
 def transform(x,y):
     r = random.random()
     if r < 0.01:
-        return 0.00 * x + 0.00 * y,  0.00 * x + 0.16 * y
-    elif r < 0.86:
+        return 0,  0.16 * y
+    elif r < 0.89:
         return 0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y - 1.60
-    elif r < 0.93:
+    elif r < 0.94:
         return 0.20 * x - 0.26 * y,  0.23 * x + 0.22 * y - 1.60
     else:
         return -0.15 * x + 0.28 * y,  0.26 * x + 0.24 * y - 0.44
@@ -36,10 +36,11 @@ while Running:
     if keys[pygame.K_LCTRL]:
         Running = False
 
-    color = (rc(),rc(),rc())
-    #color = (0,255,0)
+    #color = (rc(),rc(),rc())
+    color = (0,255,0)
 
-    x,y = transform(x,y)
-    screen.set_at((offset(x),offset(y)),color)
+    for i in range(100000):
+        x,y = transform(x,y)
+        screen.set_at((offset(x),offset(y)),color)
 
     pygame.display.flip()
